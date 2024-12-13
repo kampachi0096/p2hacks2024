1bf4ae2edf5e6af09d43b210b45b3a16bfdb1f15
kampachi0096 <kanchann033@gmail.com>
2024-12-13 12:14:50 +0900

    first commit

---
+++ facegame/facegame/facegameApp.swift
@@ -0,0 +1,17 @@
+//
+//  facegameApp.swift
+//  facegame
+//
+//  Created by kantayamamoto   on 2024/12/13.
+//
+
+import SwiftUI
+
+@main
+struct facegameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ facegame/facegameTests/facegameTests.swift
@@ -0,0 +1,17 @@
+//
+//  facegameTests.swift
+//  facegameTests
+//
+//  Created by kantayamamoto   on 2024/12/13.
+//
+
+import Testing
+@testable import facegame
+
+struct facegameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
